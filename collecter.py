79afa9cbfae774255d311999b9fcacce3e25f91d
andresvensson <andresvensson2@gmail.com>
2023-02-12 17:20:45 +0100

    dev

--- collecter.py
+++ collecter.py
@@ -1,5 +1,8 @@
-from datetime import datetime
+from datetime import datetime, timezone
 import secret
+from pyowm import OWM
+
+import pickle
 
 
 # CONSTANTS
@@ -11,27 +14,49 @@ owm_apikey = None
 if not owm_apikey:
     owm_apikey = secret.owm_apikey()
 
+own_location_id = None
+if not own_location_id:
+    own_location_id = secret.own_location_id()
+
 
 class api:
     def __init__(self) -> None:
         print("class api initiated")
-        if self.data():
-            print(self.data())
-        else:
-            print("No data to show")
 
     def data(self):
-        print("return data pls")
         # TODO
         # catch one api call and save it to a file for re-use (development)
         data = {}
+        #owm = OWM(owm_apikey)
+        #mgr = owm.weather_manager()
+        #obs = mgr.weather_at_id(own_location_id)
+        #w = obs.weather
+
+        #with open("api_data.pkl", "wb") as f:
+        #    pickle.dump(w, f)
+
+        with open('api_data.pkl', 'rb') as f:
+            w = pickle.load(f)
+
+        sunrise_date = w.sunrise_time(timeformat='date')
+        data['sunrise_date'] = sunrise_date
+        sunrise_iso = w.sunrise_time(timeformat='iso')
+        data['sunrise_iso'] = sunrise_iso
         return data
+
+    def pretty_print(self):
+        d = self.data()
+        print("data:\n", d)
+        print("date:", d['sunrise_date'])
+        print("iso:", d['sunrise_iso'])
 
 
 
 def start():
     print("Hej")
-    api()
+    a = api()
+    #a.save_apicall()
+    print(a.pretty_print())
 
 
 
